d9856d41d031c5e88b0ad44163e33c4bf23c12c2
trungbui37work-art <trungbui37work@gmail.com>
2025-09-10 21:09:52 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+base: "/trungloyal-neo/",
+  plugins: [react()],
 }));
